39d987acb7f3c03984c29d9056b6fc8088f06cfa
Hristoz Stefanov <hristoz2004@gmail.com>
2022-07-16 08:31:11 +0100

    Add dice multi-pick
    * Dice are on their own layer (to make raycasting faster)
    * We raycast a single ray capturing multiple hits
    * Only pick dice aren't holding yet

--- Assets/Scripts/Picking/Picker.cs
+++ Assets/Scripts/Picking/Picker.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+class Pickup
+{
+    public Pickable pickable; //
+    public Vector3 pickPoint; // pick up point in local space relative to pickable.transform
+    public Vector3PID holdPID = new Vector3PID(3, 3, 0.2f);
+}
+
 public class Picker : MonoBehaviour
 {
-    Pickable pickable;
-    Vector3 pickPoint;
-    bool justPicked = false;
-
-    Vector3PID pickerPID = new Vector3PID(3,3,0.2f);
+    List<Pickup> heldPickups = new List<Pickup>();
 
     // Start is called before the first frame update
     void Start()
@@ -24,31 +27,36 @@ public class Picker : MonoBehaviour
             CastRay();
         }
 
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButton(0) && Input.GetMouseButtonDown(1))
         {
-            pickable = null;
+            CastRay();
         }
 
-        if (pickable)
+        if (Input.GetMouseButtonUp(0))
         {
-            Vector3 mousePos = Input.mousePosition;
-            mousePos.z = 10;
+            heldPickups.Clear();
+        }
 
-            Vector3 newPos = Camera.main.ScreenToWorldPoint(mousePos);
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10;
 
-            var rigidBody = pickable.GetComponent<Rigidbody>();
+        Vector3 newPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        foreach (Pickup pickup in heldPickups)
+        {
+            var rigidBody = pickup.pickable.GetComponent<Rigidbody>();
             if (rigidBody)
             {
-                Vector3 force = newPos - pickable.transform.TransformPoint(pickPoint);
+                Vector3 force = newPos - pickup.pickable.transform.TransformPoint(pickup.pickPoint);
 
-                force = pickerPID.Update(force, Time.deltaTime);
+                force = pickup.holdPID.Update(force, Time.deltaTime);
 
-                rigidBody.AddForceAtPosition(force*0.01f, pickPoint);
+                rigidBody.AddForceAtPosition(force*0.01f, pickup.pickPoint);
                 rigidBody.AddForce(force);
             }
             else
             {
-                pickable.transform.position = Vector3.Lerp(pickable.transform.position, newPos, Time.deltaTime * 2.0f);
+                pickup.pickable.transform.position = Vector3.Lerp(pickup.pickable.transform.position, newPos, Time.deltaTime * 2.0f);
             }
         }
     }
@@ -66,11 +74,30 @@ public class Picker : MonoBehaviour
 
         Debug.DrawLine(rayOrigin, rayEnd, Color.red, 10);
 
-        RaycastHit hit;
-        if (Physics.Raycast(rayOrigin, direction, out hit, distance))
+        LayerMask mask = LayerMask.GetMask("Dice");
+
+        RaycastHit[] hits = Physics.RaycastAll(rayOrigin, direction, distance, mask);
+        foreach( RaycastHit hit in hits)
         {
-            pickable = hit.collider.GetComponent<Pickable>();
-            pickPoint = pickable.transform.InverseTransformPoint(hit.point);
+            if( hit.collider != null )
+            {
+                var pickable = hit.collider.GetComponent<Pickable>();
+                if (pickable != null)
+                {
+                    Pickup heldPickup = heldPickups.Find((pickup) => pickup.pickable == pickable);
+                    if (heldPickup != null)
+                    {
+                        // we're already holding this pickup check the next
+                        continue;
+                    }
+
+                    heldPickup = new Pickup();
+                    heldPickup.pickable = pickable;
+                    heldPickup.pickPoint = pickable.transform.InverseTransformPoint(hit.point);
+                    heldPickups.Add(heldPickup);
+                    return;
+                }
+            }
         }
     }
 }
